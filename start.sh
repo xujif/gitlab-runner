563ad932e51cff29a2cf817428c6ee6d4241593d
浊酒 <xujifsad@cumt.edu.cn>
2018-01-22 14:28:30 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,11 +2,11 @@
 
 file="/var/run/lock/gitlab_registered.lock"
 
-RUN_GITLAB_CI="/usr/bin/dumb-init /entrypoint run --user=$CI_RUNNER_USER --working-directory=/home/gitlab-runner"
+RUN_GITLAB_CI=/usr/bin/dumb-init /entrypoint run --user=$CI_RUNNER_USER --working-directory=/home/gitlab-runner
 
 
 if [ ! -f "$file" ]; then
-    sh -c "$PRE_INSTALL_COMMAND" && gitlab-runner register -n && touch "$file" && sh -c $RUN_GITLAB_CI
+    sh -c "$PRE_INSTALL_COMMAND" && gitlab-runner register -n && touch "$file" && sh -c "$RUN_GITLAB_CI"
 else 
-    sh -c $RUN_GITLAB_CI     
+    sh -c "$RUN_GITLAB_CI"     
 fi
