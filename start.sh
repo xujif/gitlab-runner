3e59ab81343c5f2a2a9fa8b15f10dc27714c9b48
xujif <i@xujif.com>
2018-01-22 15:16:43 +0800

    fix bug

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 file="/var/run/lock/gitlab_registered.lock"
 
-RUN_GITLAB_CI=/usr/bin/dumb-init /entrypoint run --user=$CI_RUNNER_USER --working-directory=/home/gitlab-runner
+RUN_GITLAB_CI="/usr/bin/dumb-init /entrypoint run --user=$CI_RUNNER_USER --working-directory=/home/gitlab-runner"
 
 
 if [ ! -f "$file" ]; then
